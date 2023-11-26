8d0eb7cf4b636270ee1e6d70a49ba66020e5e303
Volodya <47747293+Flexx97@users.noreply.github.com>
2023-11-26 20:07:02 +0700

    test workflow

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <h1>Our Collection</h1>
       <p>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
     </header>
-
     <view-coffee></view-coffee>
   </div>
 </template>
